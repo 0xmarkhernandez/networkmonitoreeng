cb91fc0dd65d57f8664f8e9d6be838229f064051
Mark Angelo Hernandez <markangelo.hernandez@gmail.com>
2021-06-28 11:12:13 +0400

    [CHORE] Initital code commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Mark Angelo Hernandez on 27/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
